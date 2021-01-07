cd934afa315fdc9db46868f27a325c6348e771c4
trott <trott.alik@gmail.com>
2021-01-07 17:38:00 +0500

    remove redundant proxy

--- src/telegram-bot.js
+++ src/telegram-bot.js
@@ -1,29 +1,13 @@
 const path = require('path');
 const dotenv = require('dotenv');
 const Telegraf = require('telegraf');
-const SocksAgent = require('socks5-https-client/lib/Agent');
 const decompose = require('./decompose');
 
 dotenv.config({path: path.join(__dirname, '../.env')});
 
-const {
-  TELEGRAM_DESTRUCTOR_BOT_TOKEN,
-  TELEGRAM_PROXY_HOST,
-  TELEGRAM_PROXY_PORT,
-  TELEGRAM_PROXY_USERNAME,
-  TELEGRAM_PROXY_PASSWORD
-} = process.env;
+const { TELEGRAM_DESTRUCTOR_BOT_TOKEN } = process.env;
 
-const socksAgent = new SocksAgent({
-  socksHost: TELEGRAM_PROXY_HOST,
-  socksPort: TELEGRAM_PROXY_PORT,
-  socksUsername: TELEGRAM_PROXY_USERNAME,
-  socksPassword: TELEGRAM_PROXY_PASSWORD
-});
-
-const bot = new Telegraf(TELEGRAM_DESTRUCTOR_BOT_TOKEN, {
-  telegram: {agent: socksAgent}
-});
+const bot = new Telegraf(TELEGRAM_DESTRUCTOR_BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Hello! I am a robot-decomposer. I can make factorization of your number.'));
 bot.on('message', ctx => {
