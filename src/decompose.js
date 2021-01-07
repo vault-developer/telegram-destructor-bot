d743405722456b7c9dd0298358d3e8c48983a2b2
trott <trott.alik@gmail.com>
2021-01-08 03:29:32 +0500

    update decompose.js

--- src/decompose.js
+++ src/decompose.js
@@ -1,56 +1,34 @@
-const decompose = num =>
-  new Promise((mainResolve, mainReject) => {
-    let timeout = null;
-    let cancelled = false;
+const findDivisors = num => new Promise(res => {
+    const divisors = new Map([[1,1]]);
+    let divisor = 2;
 
-    // cancel execution after timeout
-    timeout = setTimeout(() => {
-      cancelled = true
-    }, 500);
-
-    // init cancelable promise
-    const findDelimeters = num => new Promise((res, rej) => {
-      const delimeters = new Map();
-
-      delimeters.set(1, 1);
-      let delimiter = 2;
-
-      (function tryNextDelimeter() {
-        if (num % delimiter === 0) {
-          delimeters.set(delimiter, delimeters.get(delimiter) + 1 || 1);
-          num = num / delimiter;
+    (function tryNext() {
+        if (num % divisor === 0) {
+            divisors.set(divisor, (divisors.get(divisor) || 0) + 1);
+            num = num / divisor;
         } else {
-          delimiter++;
+            divisor++;
         }
-        if (delimiter <= num && !cancelled) {
-          setImmediate(tryNextDelimeter);
-        } else if (cancelled) {
-          rej();
+        if (divisor <= num) {
+            setImmediate(tryNext);
         } else {
-          clearTimeout(timeout);
-          res(delimeters)
+            res(Array.from(divisors.entries()))
         }
-      })();
-    });
+    })();
+});
 
-    // format delimeters map
-    function stringifyMap(map){
-      let result = '';
+const stringifyDivisors = divisors =>
+    divisors.reduce((acc, [key, value]) =>
+        acc.concat(`${key}${value === 1 ? '': `^${value}`} * `),'')
+            .slice(0, -2);
 
-      for (let [key, value] of map) {
-        if (value === 1) {
-          result = result.concat(`${key} * `);
-        } else {
-          result = result.concat(`${key}^${value}* `);
-        }
-      }
-      return result.slice(0, -2);
-    }
+const getStringifiedDivisors = async num =>{
+    const divisors = await findDivisors(num);
+    return stringifyDivisors(divisors);
+}
 
-    // resolve promise
-    findDelimeters(num)
-      .then(res => mainResolve(stringifyMap(res)))
-      .catch(() => mainReject('It\'s too difficult for me =('));
-  });
+const getTimeoutError = ms =>
+    new Promise((_, rej) =>
+        setTimeout(()=>rej('It\'s too difficult for me =('), ms));
 
-module.exports = decompose;
+module.exports = num => Promise.race([getStringifiedDivisors(num), getTimeoutError(500)]);
